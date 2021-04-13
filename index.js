437d79c7fb96ff52f21aaa656c31c5958145ef50
MrFarhan <muhammadfarhan012345@gmail.com>
2021-04-14 01:20:46 +0500

    two queries at a time

--- index.js
+++ index.js
@@ -4,24 +4,35 @@ const { graphqlHTTP } = require('express-graphql');
 
 
 const app = express();
- 
+
 var schema = buildSchema(`
   type Query {
     hello: String
+    getPost : Post
+  }
+  type Post {
+      id: Int
+      title:String
   }
 `);
- 
-var root = { hello: () => 'Hello world!' };
- 
+
+var root = {
+    hello: () => 'Hello world!',
+    getPost: () => ({
+        id: "1",
+        title: "temp title here"
+    })
+};
+
 app.use(
-  '/graphql',
-  graphqlHTTP({
-    schema,
-    rootValue:root,
-    graphiql:true,
-  }),
+    '/graphql',
+    graphqlHTTP({
+        schema,
+        rootValue: root,
+        graphiql: true,
+    }),
 );
 
-app.listen(4000,()=>{
-console.log("server is listning at 4000")
+app.listen(4000, () => {
+    console.log("server is listning at 4000")
 });
